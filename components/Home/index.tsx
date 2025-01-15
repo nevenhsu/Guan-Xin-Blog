bb43262193a00fa7aef5d89ff6ec178baaaf920b
Neven <space.man0318@gmail.com>
2025-01-15 16:40:52 +0800

    add carousel

--- components/Home/index.tsx
+++ components/Home/index.tsx
@@ -1,7 +1,50 @@
+'use client'
+
+import Image from 'next/image'
+import { Center, Title } from '@mantine/core'
+import { Carousel } from '@mantine/carousel'
+import classes from './index.module.css'
+
+const MySlide = Center.withProps({ h: '100%', className: classes.slide })
+const MyTitle = Title.withProps({
+  px: 60,
+  style: {
+    zIndex: 1,
+    color: 'var(--mantine-color-white)',
+    textShadow: '2px 2px 1px rgba(0, 0, 0, 0.50)',
+    textAlign: 'center',
+  },
+})
+
 export default function Home() {
   return (
-    <div>
-      <h1>Home</h1>
-    </div>
+    <>
+      <Carousel withIndicators loop height={400}>
+        <Carousel.Slide>
+          <MySlide>
+            <Image src="/images/slide-0.png" alt="" fill />
+            <MyTitle>Title 1</MyTitle>
+          </MySlide>
+        </Carousel.Slide>
+        <Carousel.Slide>
+          <MySlide>
+            <Image src="/images/slide-1.png" alt="" fill />
+            <MyTitle>Title 2</MyTitle>
+          </MySlide>
+        </Carousel.Slide>
+        <Carousel.Slide>
+          <MySlide>
+            <Image src="/images/slide-2.png" alt="" fill />
+            <MyTitle>Title 3</MyTitle>
+          </MySlide>
+        </Carousel.Slide>
+        <Carousel.Slide>
+          <MySlide>
+            <Image src="/images/slide-3.png" alt="" fill />
+            <MyTitle>Title 4</MyTitle>
+          </MySlide>
+        </Carousel.Slide>
+      </Carousel>
+    </>
   )
 }
